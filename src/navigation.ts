cc340fc434ee437f26a4e5f341917aca4fc9ce44
Harley <harley.cosio@gmail.com>
2025-02-11 16:50:07 -0500

    changed mayor index page content

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,9 +1,10 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { getPermalink, getBlogPermalink } from './utils/permalinks';
 
 export const headerData = {
   links: [
     {
       text: 'Home',
+      href: getPermalink('/'),
      /* links: [
         {
           text: 'SaaS',
@@ -57,7 +58,7 @@ export const headerData = {
       ],
     },
     {
-      text: 'Landing',
+      text: 'About us',
       links: [
         {
           text: 'Lead Generation',
@@ -117,7 +118,7 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: 'Product',
+      title: 'Product', 
       links: [
         { text: 'Features', href: '#' },
         { text: 'Security', href: '#' },
@@ -166,14 +167,13 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
+    { ariaLabel: 'Reddit', icon: 'tabler:brand-reddit', href: 'https://github.com/onwidget/astrowind' },   
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    Made with <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a>
   `,
 };
